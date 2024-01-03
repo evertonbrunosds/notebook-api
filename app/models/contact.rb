b9d20723ce2a10739c5fc98ec3cff99a54948992
evertonbrunosds <evertonbrunosds@gmail.com>
2024-01-03 01:36:55 -0300

    v1.0 - render JSON com associações

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,11 @@
 class Contact < ApplicationRecord
     belongs_to :kind
     def as_json(options={})
-        super(except: [:created_at, :updated_at], root: true)
+        super(
+            except: [:created_at, :updated_at, :kind_id],
+            include: {
+                kind: {except: [:created_at, :updated_at]}
+            }
+        )
     end
 end
