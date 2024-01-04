c73ea01aa292919a6d4672106823456c12d572a8
evertonbrunosds <evertonbrunosds@gmail.com>
2024-01-03 21:08:00 -0300

    v1.7 - aplicando o I18n as datas

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,13 +2,15 @@ class Contact < ApplicationRecord
     belongs_to :kind
     has_many :phones, dependent: :destroy
 
-    def as_json()
-        super(
-            except: %i[ created_at updated_at kind_id ],
+    def as_json
+        json = super(
+            except: %i[ created_at updated_at kind_id birthdate ],
             include: {
                 kind: {except: %i[ created_at updated_at ]}
             }
         )
+        json[:birthdate] = (I18n.l(birthdate) unless birthdate.blank?)
+        json
     end
 
 end
