6805757d0075459b5f5ca235810e23c621c198eb
evertonbrunosds <evertonbrunosds@gmail.com>
2024-01-05 14:38:20 -0300

    v3.0 - configurando 'MEDIA TYPE'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+    before_action :ensure_json_request
+    before_action :set_json_api_content_type
+
+    def ensure_json_request
+        return if request.headers['Accept'] =~ /application\/vnd\.api\+json/
+        render :nothing => true, :status => 406
+    end
+
+    private
+
+    def set_json_api_content_type
+        response.headers['Content-Type'] = 'application/vnd.api+json'
+    end
+
 end
