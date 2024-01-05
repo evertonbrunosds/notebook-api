97233ebd11d77898073dbbff82113f949c20d57c
evertonbrunosds <evertonbrunosds@gmail.com>
2024-01-04 22:45:00 -0300

    v2.5 - utilizando especificação AMS

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,10 +5,4 @@ class Contact < ApplicationRecord
     accepts_nested_attributes_for :phones, allow_destroy: true
     accepts_nested_attributes_for :address, update_only: true #IMPEDE A CRIAÇÃO DE NOVOS REGISTROS (ATUA COMO UMA RESTRIÇÃO UNIQUE)
 
-    def as_json(options={})
-        json = super(options)
-        json[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
-        json
-    end
-
 end
--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,3 +1,11 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes %i[ id name email birthdate ]
+
+  def attributes(*args)
+    hash = super(*args)
+    # pt-BR ---> hash[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
+    hash[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
+    hash
+  end
+
 end
