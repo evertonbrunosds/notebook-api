c15ce80d116aa61383f817eba2e3c775ba8ee6c6
evertonbrunosds <evertonbrunosds@gmail.com>
2024-01-08 22:16:16 -0300

    v4.7 - erros especificados com JSON API

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,17 @@
 class Contact < ApplicationRecord
+    
+    #Associations
     belongs_to :kind
     has_many :phones, dependent: :destroy
     has_one :address, dependent: :destroy
     accepts_nested_attributes_for :phones, allow_destroy: true
     accepts_nested_attributes_for :address, update_only: true #IMPEDE A CRIAÇÃO DE NOVOS REGISTROS (ATUA COMO UMA RESTRIÇÃO UNIQUE)
 
+    #Validations
+    validates_presence_of :kind
+    validates_presence_of :address
+
+    #Paginations
     paginates_per 5
 
 end
